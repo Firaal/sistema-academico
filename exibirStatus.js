bcae6ac2e071e0709efa82fdc24bebe1422325b3
ManueleMacêdo <manuelemacedo.ads@gmail.com>
2024-11-12 13:17:49 -0300

    Commit inicial - Implementação dos módulos e testes

---
+++ exibirStatus.js
@@ -0,0 +1,6 @@
+function obterStatus(media) {
+  if (media < 4.0) return 'Reprovado por média';
+  if (media >= 6.0) return 'Aprovado por média';
+  return 'Aguardando a final';
+}
+module.exports = obterStatus;
